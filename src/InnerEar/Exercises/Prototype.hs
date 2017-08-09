d5f8b9411b2304960110406eaf59f2329fd08212
d0kt0r0 <ogbornd@mcmaster.ca>
2017-08-09 13:58:08 -0400

    prototypeExercise fleshed out a bit

--- src/InnerEar/Exercises/Prototype.hs
+++ src/InnerEar/Exercises/Prototype.hs
@@ -10,5 +10,9 @@ prototypeExercise :: MonadWidget t m
   => Event t Response -> m (Event t Request,Event t ())
 prototypeExercise responses = el "div" $ do
   text "prototype exercise placeholder"
+  makeASound <- liftM ((Synth (PinkNoise 2.0) (PeakingFilter 1000.0 1.0 1.0)) <$) $ button "Make A Sound"
+  performSynth makeASound
+  score <- count makeASound
+  drawBar score -- drawBar :: (MonadWidget t m, Num b) => Dynamic t b -> m () -- MonadWidget t m => Dynamic t Int -> m ()
   home <- button "back to splash page"
   return (never,home)
