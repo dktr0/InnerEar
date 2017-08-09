bdebe82627fb24e5d88c6987f29256a73af7adcc
luis <luisnavarrodelangel@gmail.com>
2017-08-09 18:27:35 -0400

    first_bar

--- src/InnerEar/Exercises/Prototype.hs
+++ src/InnerEar/Exercises/Prototype.hs
@@ -7,16 +7,16 @@ import Control.Monad
 import InnerEar.Types.Request
 import InnerEar.Types.Response
 import Reflex.Synth.Synth
-
+import InnerEar.Widgets.Test
 
 prototypeExercise :: MonadWidget t m
   => Event t Response -> m (Event t Request,Event t ())
 prototypeExercise responses = el "div" $ do
-  text "prototype exercise placeholder"
+  text "prototype exercise placehoder"
   makeASound <- liftM ((Synth (PinkNoise 2.0) (PeakingFilter 1000.0 1.0 1.0)) <$) $ button "Make A Sound"
   performSynth makeASound
   score <- count makeASound
-  drawBar score -- drawBar :: (MonadWidget t m, Num b) => Dynamic t b -> m () -- MonadWidget t m => Dynamic t Int -> m ()
+  drawbar score -- drawBar :: (MonadWidget t m, Num b) => Dynamic t b -> m () -- MonadWidget t m => Dynamic t Int -> m ()
   home <- button "back to splash page"
   return (never,home)
 
--- src/InnerEar/Widgets/Test.hs
+++ src/InnerEar/Widgets/Test.hs
@@ -1,12 +1,19 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE OverloadedLists       #-}
+{-# LANGUAGE TemplateHaskell       #-}
+
 module InnerEar.Widgets.Test where
 
 import Reflex
 import Reflex.Dom
+import Data.Map (Map)
+import qualified Data.Map as M
+import Data.FileEmbed
 
 import InnerEar.Types.Request
 import InnerEar.Types.Response
 
-import qualified Data.Map as M
+
 
 testWidget :: MonadWidget t m
   => Event t Response -> m (Event t Request,Event t ())
@@ -15,11 +22,16 @@ testWidget responses = el "div" $ do
   home <- button "back to splash page"
   return (never,home)
 
-drawbar ::  MonadWidget t m => Dynamic t Int -> m ()
-drawbar score = do
-  let svg = el "svg"
-  let svgAttrs :: Map String String
-  let svgAttrs = [("width", "300px")
-                 ,("height","300px")]
-      svg $ svgAttrs $ score $ do
-       el "rect" $ M.fromList [("width", "100px"), ("height", "100px"), ("fill", "blue")]
+drawbar ::  MonadWidget t m =>  Dynamic t Int -> m ()
+drawbar x =  do
+ let svg = Just "http://www.w3.org/2000/svg"
+ let svgAttrs :: Map String String
+     svgAttrs = [("width", "100px")
+                , ("height", "200px")
+                , ("viewBox", "0 0 300 200") ]
+ --elDynAttr "200px" svgAttrs $ do el "height" $ x
+ elWith "svg" (ElConfig svg svgAttrs) $ do
+   elWith "rect" (ElConfig svg (M.fromList [("width", "100%"), ("height", "100%"), ("fill", "red")])) (return ())
+
+
+
