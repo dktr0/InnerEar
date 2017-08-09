79dc1c75a37cbd2543289a48ad81b2416901776e
d0kt0r0 <ogbornd@mcmaster.ca>
2017-08-09 15:13:50 -0400

    oops fixed stack.yaml accident

--- src/InnerEar/Types/Response.hs
+++ src/InnerEar/Types/Response.hs
@@ -5,10 +5,13 @@ import InnerEar.Types.Utility
 import InnerEar.Types.Handle
 
 data Response =
-  Authenticated Handle -- signals that client is successfully authenticated as the indicated handle
+  Authenticated Handle | -- signals that client is successfully authenticated as the indicated handle
+  Downloaded Record
 
 instance JSON Response where
   showJSON (Authenticated h) = encJSDict [("Authenticated",h)]
+  showJSON (Downloaded r) = encJSDict [("Downloaded",r)]
   readJSON (JSObject x) | firstKey x == "Authenticated" = Authenticated <$> valFromObj "Authenticated" x
+  readJSON (JSObject x) | firstKey x == "Downloaded" = Downloaded <$> valFromObj "Downloaded" x
   readJSON (JSObject x) | otherwise = Error $ "Unable to parse JSOBject as Response" ++ (show x)
   readJSON _ = Error "Unable to parse non-JSObject as Response"
