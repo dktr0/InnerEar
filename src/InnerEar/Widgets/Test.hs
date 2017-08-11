4d0e6ff21f145e2facc01b9013ae41a4edb6fd3e
Jamie Beverley <jamie_beverley@hotmail.com>
2017-08-11 13:13:38 -0400

    10 bands exercise added

--- src/InnerEar/Widgets/Test.hs
+++ src/InnerEar/Widgets/Test.hs
@@ -41,13 +41,12 @@ testSoundWidget _ = el "div" $ do
   return (never,home)
 
 
-drawBar ::  MonadWidget t m =>  Dynamic t Int -> m ()
-drawBar x =  do
- let svg = Just "http://www.w3.org/2000/svg"
- let svgAttrs :: Map String String
-     svgAttrs = [("width", "100px")
-                , ("height", "200px")
-                , ("viewBox", "0 0 300 200") ]
- --elDynAttr "200px" svgAttrs $ do el "height" $ x
- elWith "svg" (ElConfig svg svgAttrs) $ do
-   elWith "rect" (ElConfig svg (M.fromList [("width", "100%"), ("height", "100%"), ("fill", "red")])) (return ())
+--drawBar ::  MonadWidget t m =>  Dynamic t Int -> m ()
+--drawBar x =  do
+-- let svg = Just "http://www.w3.org/2000/svg"
+-- let svgAttrs = fromList [("width", "100px")
+--                , ("height", "200px")
+--                , ("viewBox", "0 0 300 200") ]
+-- --elDynAttr "200px" svgAttrs $ do el "height" $ x
+-- elWith "svg" (ElConfig svg svgAttrs) $ do
+--   elWith "rect" (ElConfig svg (M.fromList [("width", "100%"), ("height", "100%"), ("fill", "red")])) (return ())
