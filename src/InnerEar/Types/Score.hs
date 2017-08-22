c2f625ce31651a3369faa006b65dae49fe15ed55
Jamie Beverley <jamie_beverley@hotmail.com>
2017-08-22 11:51:19 -0400

    now deriving Data and Typeable

--- src/InnerEar/Types/Score.hs
+++ src/InnerEar/Types/Score.hs
@@ -1,12 +1,16 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+
 module InnerEar.Types.Score where
 
 
+import Text.JSON
+import Text.JSON.Generic
 
 data Score = Score { 
   questionsAsked::Int, -- times user has been tested on this option (times asked)
   falsePositives::Int, -- times user thinks it is this the answer but correct answer is something else
   falseNegatives::Int  -- times user thinks it is another answer but this is the correct answer
-} deriving (Show, Eq)
+} deriving (Show, Eq,Typeable,Data)
 
 data ScorePossibility = Correct | FalsePositive | FalseNegative deriving (Show, Eq)
 
