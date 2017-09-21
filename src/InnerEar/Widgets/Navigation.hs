2306c21dfe8946bc4533a142683927f69c9f138d
David Ogborn <ogbornd@mcmaster.ca>
2017-09-21 16:24:58 -0400

    added rough template of LeftRightCentre (panning) exercise

--- src/InnerEar/Widgets/Navigation.hs
+++ src/InnerEar/Widgets/Navigation.hs
@@ -29,6 +29,7 @@ import InnerEar.Exercises.TenBandBoostCut
 import InnerEar.Exercises.AddedWhiteNoise
 import InnerEar.Exercises.RT60
 import InnerEar.Exercises.Compression
+import InnerEar.Exercises.LeftRightCentre
 
 data Navigation =
   SplashPage |
@@ -54,7 +55,8 @@ includedExercises = [
   FiveBandBoostCut,
   AddedWhiteNoise,
   RT60,
-  Compression
+  Compression,
+  LeftRightCentre
   ]
 
 buttonForExercise :: MonadWidget t m => ExerciseId -> m (Event t Navigation)
@@ -93,6 +95,8 @@ navigationPage responses areTheyAuthenticated (ExercisePage RT60) =
   runExerciseForNavigationPage rt60Exercise responses areTheyAuthenticated
 navigationPage responses areTheyAuthenticated (ExercisePage Compression) =
   runExerciseForNavigationPage compressionExercise responses areTheyAuthenticated
+navigationPage responses areTheyAuthenticated (ExercisePage LeftRightCentre) =
+  runExerciseForNavigationPage leftRightCentreExercise responses areTheyAuthenticated
 
 navigationPage responses areTheyAuthenticated TestPage = do
   (requests,sounds,navUnit) <- testWidget responses
