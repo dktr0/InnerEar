fcdf35d9ee7d7dc134a46ea6fc2dab0a1d796e9b
luis <luisnavarrodelangel@gmail.com>
2018-02-01 13:02:44 -0500

    updates

--- src/InnerEar/Widgets/Bars.hs
+++ src/InnerEar/Widgets/Bars.hs
@@ -6,132 +6,15 @@ import Data.Map
 import Reflex.Dom.Contrib.Widgets.Svg
 import Control.Monad
 import Data.Maybe (isJust)
-
 import InnerEar.Widgets.Utility
 import InnerEar.Widgets.Labels
 import InnerEar.Types.Score
 
--- a svg with dynamic CSS and height
-dynSvgCss :: MonadWidget t m => Dynamic t String -> Dynamic t Float -> m ()
-dynSvgCss cssClass height = do
-  cssClass' <- mapDyn (singleton "class") cssClass
-  height' <- mapDyn (singleton "height" . show) height
-  attrs <- mconcatDyn [cssClass', height']
-  svgDynAttr "svg" attrs $ return ()
-
---Dynamic "rect" element
-rect :: MonadWidget t m => Dynamic t Int -> Dynamic t Int -> Dynamic t Float -> Dynamic t Float -> Dynamic t String -> Dynamic t String -> m ()
-rect posX posY width height style transform= do
-  posX' <- mapDyn (singleton "x" . show) posX
-  posY' <- mapDyn (singleton "y" . show) posY
-  width' <- mapDyn (singleton "width" . show) width
-  height' <- mapDyn (singleton "height" . show) height
-  style' <- mapDyn (singleton "style") style
-  transform' <- mapDyn (singleton "transform") transform
-  m <- mconcatDyn [posX', posY', width', height', style', transform']
-  svgDynAttr "rect" m $ return()
-
---Dynamic "rect" element with CSS style
-rectDynCSS :: MonadWidget t m => Dynamic t Int -> Dynamic t Int -> Dynamic t Double -> Dynamic t Double -> Dynamic t String -> Dynamic t String -> m ()
-rectDynCSS posX posY width height transform cssClass = do
-    cssClass' <- mapDyn (singleton "class") cssClass
-    posX' <- mapDyn (singleton "x" . show) posX
-    posY' <- mapDyn (singleton "y" . show) posY
-    height' <- mapDyn (singleton "height" . show) height
-    width' <- mapDyn (singleton "width" . show) width
-    transform' <- mapDyn (singleton "transform") transform
-    m <- mconcatDyn [posX', posY', width',height', cssClass', transform']
-    svgDynAttr "rect" m $ return ()
-
---A dynamic bar
-drawBar' :: MonadWidget t m =>  Dynamic t Float -> m ()
-drawBar' x  = do
-    let m = fromList [("width","200px"),("height","200px"), ("viewBox", "0 0 300 200")]
-    svgAttr "svg" m $ do
-       let posX = constDyn  20 -- $ negate 100
-       let posY = constDyn  50 -- $ negate 200
-       let w = constDyn 50
-       h <- mapDyn (*5) x
-       let t = constDyn "rotate(0)"
-       let s = constDyn "fill:green;stroke-width:5"
-       rect posX posY w h s t
-
---A dynamic bar with css style and in-line attributes
-drawBarCSS :: MonadWidget t m =>  Dynamic t Double -> Dynamic t Double -> m ()
-drawBarCSS x y = do
-    svgClass "svg" "svgForBars" $ do
-      let posX = constDyn $ negate 30
-      let posY = constDyn $ negate 200
-      h <- mapDyn (*100) x
-      w <- mapDyn (*1) y
-      let c = constDyn "test"
-      let t = constDyn "rotate (180)"
-      rectDynCSS posX posY w h t c
-
---A dynamic bar with css style and in-line attributes
-drawBarCSS'' :: MonadWidget t m =>  Dynamic t Double -> Dynamic t Double -> m ()
-drawBarCSS'' x y = do
-    let class' = constDyn (singleton "class" "svgForBars")
-    height' <- mapDyn (* 200) x
-    height'' <- mapDyn (singleton "height" . show) height'
-    attrs <- mconcatDyn [class', height'']
-    svgDynAttr "svg" attrs $ do
-       let posX = constDyn $ negate 30
-       let posY = constDyn $ negate 200
-       h <- mapDyn (*200) x
-       w <- mapDyn (*1) y
-       let c = constDyn "test"
-       let t = constDyn "rotate (180)"
-       rectDynCSS posX posY w h t c
-
---A dynamic bar with a label, a button and CSS style
-labelBarButton :: MonadWidget t m => String ->  Dynamic t String -> Dynamic t Double -> m (Event t ())
-labelBarButton label buttonString barHeight = do
-   labelsForBars label
-   let barWidth = constDyn 30
-   drawBarCSS barHeight barWidth
-   question <- dynButton buttonString -- m (Event t ())
-   return (question)
-
---A dynamic bar with a label, maybe percentage, maybe button label and maybe height
-dynLabelBarButton :: MonadWidget t m => String ->  Dynamic t (Maybe Int) ->  Dynamic t (Maybe String) -> Dynamic t (Maybe Double) -> m (Event t ())
-dynLabelBarButton label p buttonString barHeight = elClass "div" "barWrapper" $ do
-    labelsForBars label
-    let barWidth = constDyn 30
-    boolBar <- mapDyn (maybe False (const True)) barHeight
-    barHeight' <- mapDyn (maybe 0.0 id) barHeight
-    flippableDyn (text " ") (drawBarCSS barHeight' barWidth) boolBar
-    boolPercentage <- mapDyn (maybe False (const True)) p
-    p' <- mapDyn (maybe 0 id) p -- Dynamic t Int
-    flippableDyn (text " ") (dynPercentage p') boolPercentage
-    el "div" $ do
-      buttonString' <- mapDyn (maybe " " id) buttonString
-      boolButton <- mapDyn (maybe False (const True)) buttonString --Dynamic t bool
-      let emptyString = constDyn " "
-      flippableDynE (dynButton emptyString) (dynButton buttonString') boolButton
-
-performanceBar :: MonadWidget t m => Dynamic t Float -> Dynamic t String -> Dynamic t Int -> m ()
-performanceBar percentage label count =  do
-  dynPercentageFloat (constDyn "percentageClass") percentage
-  dynLabelForBar (constDyn "dynLabelForBarClass") label
-  dynCount (constDyn "dynCountClass") count
-
-  --A dynamic bar with css style and in-line attribute
-dynBarCSS :: MonadWidget t m =>  Dynamic t Double -> Dynamic t Double -> m ()
-dynBarCSS percent barWidth = do
-      svgClass "svg" "svgBarContainer" $ do
-        let posX = constDyn $ negate 30 -- Dynamic t Int
-        let posY = constDyn $ negate 200  --Dynamic t Int
-        barHeight' <- mapDyn (*200) percent -- m (Dynamic t Float)
-        barWidth' <- mapDyn (*1) barWidth -- m (Dynamic t Float)
-        let c = constDyn "bars" --Dynamic t String
-        let t = constDyn "rotate (180)" --Dynamic t String
-        rectDynCSS posX posY barWidth' barHeight' t c  -- m ()
-
-dynBarCSS' :: MonadWidget t m =>  Dynamic t Double -> Dynamic t Float -> m ()
-dynBarCSS' percent barWidth =  do
-    let class' = constDyn (singleton "class" "svgBarContainer")
-    svgHeight <- mapDyn (* 100) percent
+dynBarCSS' :: MonadWidget t m =>  Dynamic t Double -> Dynamic t Float -> Dynamic t String -> m ()
+dynBarCSS' percent barWidth c =  do
+    class' <- mapDyn (singleton "class") c
+    --let class' = constDyn (singleton "class" "svgBarContainer")
+    svgHeight <- mapDyn (* 150) percent
     svgHeight' <- mapDyn (singleton "height" . show) svgHeight
     let rotate' = constDyn (singleton "transform" "rotate (180)")
     attrs <- mconcatDyn [class', svgHeight', rotate']
@@ -159,9 +42,9 @@ scoreBar key score  = elClass "div" "scoreBarWrapper" $ do
   bool <-  mapDyn (maybe False (const True)) score
   score' <-  mapDyn (maybe (Score 0 0 0) id) score -- Dynamic t Int
   percent <- mapDyn asPercent score'
-  let b = dynScoreLabel (constDyn "scoreLabel") percent >> dynBarCSS' percent (constDyn 100)
+  let b = dynScoreLabel (constDyn "scoreLabel") percent >> dynBarCSS' percent (constDyn 100) (constDyn "svgBarContainer")
   flippableDyn (return ()) b bool
-  let b2 = emptyScoreLabel >> faintedLineCSS "svgFaintedLine" >> dynBarCSS' percent (constDyn 100)
+  let b2 = emptyScoreLabel >> faintedLineCSS "svgFaintedLine" >> dynBarCSS' percent (constDyn 100) (constDyn "svgBarContainer")
   flippableDyn b2 (return ()) bool
   faintedLineToAdjustGraph "faintedLineToAdjustGraph"
   xLabel "xLabel" key
@@ -172,12 +55,12 @@ scoreBar' key score  = elClass "div" "scoreBarWrapperFiveBand" $ do
     bool <-  mapDyn (maybe False (const True)) score
     score' <-  mapDyn (maybe (Score 0 0 0) id) score -- Dynamic t Int
     percent <- mapDyn asPercent score'
-    let b = dynScoreLabel (constDyn "scoreLabel") percent >> dynBarCSS' percent (constDyn 100)
+    let b = dynScoreLabel (constDyn "scoreLabel") percent >> dynBarCSS' percent (constDyn 100) (constDyn "svgBarContainerFiveBand")
     flippableDyn (return ()) b bool
-    let b2 = emptyScoreLabel >> faintedLineCSS "svgFaintedLine" >> dynBarCSS' percent (constDyn 100)
+    let b2 = emptyScoreLabel >> faintedLineCSS "svgFaintedLineFiveBand" >> dynBarCSS' percent (constDyn 100) (constDyn "svgBarContainerFiveBand")
     flippableDyn b2 (return ()) bool
     faintedLineToAdjustGraph "faintedLineToAdjustGraph"
-    xLabel "xLabel" key
+    xLabel "xLabelFiveBand" key
     mapDyn questionsAsked score' >>= dynCountLabel (constDyn "countLabel")
 
 scoreBar'' :: MonadWidget t m => String -> Dynamic t (Maybe Score) -> m ()
@@ -185,9 +68,9 @@ scoreBar'' key score  = elClass "div" "scoreBarWrapperTenBand" $ do
     bool <-  mapDyn (maybe False (const True)) score
     score' <-  mapDyn (maybe (Score 0 0 0) id) score -- Dynamic t Int
     percent <- mapDyn asPercent score'
-    let b = dynScoreLabel (constDyn "scoreLabel") percent >> dynBarCSS' percent (constDyn 100)
+    let b = dynScoreLabel (constDyn "scoreLabel") percent >> dynBarCSS' percent (constDyn 100) (constDyn "svgBarContainerTenBand")
     flippableDyn (return ()) b bool
-    let b2 = emptyScoreLabel >> faintedLineCSS "svgFaintedLine" >> dynBarCSS' percent (constDyn 100)
+    let b2 = emptyScoreLabel >> faintedLineCSS "svgFaintedLineTenBand" >> dynBarCSS' percent (constDyn 100) (constDyn "svgBarContainerTenBand")
     flippableDyn b2 (return ()) bool
     faintedLineToAdjustGraph "faintedLineToAdjustGraph"
     xLabel "xLabelTenBand" key
--- src/InnerEar/Widgets/SpecEval.hs
+++ src/InnerEar/Widgets/SpecEval.hs
@@ -11,20 +11,6 @@ import InnerEar.Widgets.Bars
 import InnerEar.Types.Score
 import InnerEar.Widgets.Labels
 
-
-displaySpectrumEvaluation :: MonadWidget t m => Dynamic t String -> Dynamic t (Map Frequency Score) -> m ()
-displaySpectrumEvaluation graphLabel score = elClass "div" "specEvalWrapper" $ do
-  dynGraphLabel (constDyn "graphLabel") graphLabel
-  maybeScore<- mapDyn (mapKeys (freqAsString) . fmap (\v-> case v of (Score 0 0 0)-> Nothing;otherwise->Just v)) score
-  listWithKey maybeScore scoreBar
-  return ()
-
-{-
-displayMultipleChoiceEvaluation :: (MonadWidget t m, Show a, Ord a)
-  => String -> String -> [a] -> Dynamic t (Map a Score) -> m ()
-displayMultipleChoiceEvaluation graphLabel xLabel possibilities scoreMap = ...
--}
-
 evalGraphFrame :: MonadWidget t m =>  String -> String -> m ()
 evalGraphFrame xMainLabel graphLabel = do
   faintedXaxis "faintedXaxis"
@@ -33,62 +19,6 @@ evalGraphFrame xMainLabel graphLabel = do
   countMainLabel "countMainLabel" "#"
   percentageMainLabel "percentageMainLabel" "%"
   elClass "div" "graphLabel" $ text graphLabel
-  return ()
-
-
-displayCurrentSpectrumEvaluation :: MonadWidget t m => String -> Dynamic t (Map Frequency Score) -> m ()
-displayCurrentSpectrumEvaluation graphLabel score = elClass "div" "specEvalWrapper" $ do
-  evalGraphFrame "Hz" graphLabel
-  let labels = ["31","63","125","250","500","1","2","4","8","16"]
-  let frequencies = zipWith F [31::Double,63,125,250,500,1000,2000,4000,8000,16000] labels -- [Frequency]
-  let band0Hz = (!!0) labels -- String
-  let band1Hz = (!!1) labels
-  let band2Hz = (!!2) labels
-  let band3Hz = (!!3) labels
-  let band4Hz = (!!4) labels
-  let band5Hz = (!!5) labels
-  let band6Hz = (!!6) labels
-  let band7Hz = (!!7) labels
-  let band8Hz = (!!8) labels
-  let band9Hz = (!!9) labels
-
-  band0Score <- mapDyn (Data.Map.lookup (frequencies!!0)) score --  Dynamic t (Score) ?
-  band1Score <- mapDyn (Data.Map.lookup (frequencies!!1)) score
-  band2Score <- mapDyn (Data.Map.lookup (frequencies!!2)) score
-  band3Score <- mapDyn (Data.Map.lookup (frequencies!!3)) score
-  band4Score <- mapDyn (Data.Map.lookup (frequencies!!4)) score
-  band5Score <- mapDyn (Data.Map.lookup (frequencies!!5)) score
-  band6Score <- mapDyn (Data.Map.lookup (frequencies!!6)) score
-  band7Score <- mapDyn (Data.Map.lookup (frequencies!!7)) score
-  band8Score <- mapDyn (Data.Map.lookup (frequencies!!8)) score
-  band9Score <- mapDyn (Data.Map.lookup (frequencies!!9)) score
-
-  band0ScoreBar <- scoreBar band0Hz band0Score-- m ()
-  band1ScoreBar <- scoreBar band1Hz band1Score
-  band2ScoreBar <- scoreBar band2Hz band2Score
-  band3ScoreBar <- scoreBar band3Hz band3Score
-  band4ScoreBar <- scoreBar band4Hz band4Score
-  band5ScoreBar <- scoreBar band5Hz band5Score
-  band6ScoreBar <- scoreBar band6Hz band6Score
-  band7ScoreBar <- scoreBar band7Hz band7Score
-  band8ScoreBar <- scoreBar band8Hz band8Score
-  band9ScoreBar <- scoreBar band9Hz band9Score
-  return ()
-
-displayMultipleChoiceEvaluationGraph :: (MonadWidget t m, Show a, Ord a) => String -> String -> [a] -> Dynamic t (Map a Score) -> m ()
-displayMultipleChoiceEvaluationGraph graphLabel xLabel possibilities scoreMap = elClass "div" "specEvalWrapper" $ do
-  elClass "div" "graphLabel" $ text graphLabel
-  elClass "div"  "xLabel" $ text xLabel
---  questionScore <- mapDyn (mapKeys a) score
-  let questionLabel1 = show $ (!!0) possibilities
-  let questionLabel2 = show $ (!!1) possibilities
-
-  questionScore1 <- mapDyn (Data.Map.lookup (possibilities!!0)) scoreMap
-  questionScore2 <- mapDyn (Data.Map.lookup (possibilities!!1)) scoreMap
-
-  questionScoreBar1 <- scoreBar questionLabel1 questionScore1
-  questionScoreBar2 <- scoreBar questionLabel2 questionScore2
-
   return ()
 
 displayMultipleChoiceEvaluationGraph' :: (MonadWidget t m, Show a, Ord a) => String -> String -> [a] -> Dynamic t (Map a Score) -> m ()
@@ -109,14 +39,6 @@ displayMultipleChoiceEvaluationGraph'' graphLabel qLabel possibilities scoreMap 
       return ()
       where f k d = scoreBar' (show k) d
 
-{- x graphLabel qLabel possibilities scoreMap = do
- listOfScores <- mapDyn ...
- let qLabel' = Line 400 300 200 200
- let possibilities = Rect 400 300 200 100
- scores <- mapDyn (\x -> Line x 300 150 100) listOfScores
- allDrawingInstructions <- mapDyn (\x -> qLabel':possibilities:x) scores
- dynSvg allDrawingInstructions -}
-        
 displayMultipleChoiceEvaluationGraph''' :: (MonadWidget t m, Show a, Ord a) => String -> String -> [a] -> Dynamic t (Map a Score) -> m ()
 displayMultipleChoiceEvaluationGraph''' graphLabel qLabel possibilities scoreMap = elClass "div" "specEvalWrapper" $ do
       scoreList <- mapDyn (\x -> fmap (\y -> Data.Map.lookup y x) possibilities) scoreMap -- m (Dynamic t [Maybe Score])
@@ -126,15 +48,11 @@ displayMultipleChoiceEvaluationGraph''' graphLabel qLabel possibilities scoreMap
       return ()
       where f k d = scoreBar'' (show k) d
 
-{-
-displaySpectrumEvaluationGraphs :: MonadWidget t m => m ()
-displaySpectrumEvaluationGraphs = do
-  --  let currentM = constDyn (Data.Map.fromList [((F 31 "31"), (Score 1 0 9)), ((F 63 "63"), (Score 2 0 8)), ((F 125 "125"), (Score 3 0 7)), ((F 250 "250"), (Score 4 0 6)), ((F 500 "500"), (Score 5 0 5)), ((F 1000 "1"), (Score 6 0 4)), ((F 2000 "2"), (Score 7 0 3)), ((F 4000 "4"), (Score 8 0 2)), ((F 8000 "8"), (Score 9 0 1)), ((F 16000 "16"), (Score 10 0 0))])
-  --  let currentM = constDyn (Data.Map.fromList [((F 125 "125"), (Score 3 0 7)), ((F 250 "250"), (Score 4 0 6)), ((F 500 "500"), (Score 5 0 5)), ((F 1000 "1"), (Score 6 0 4)), ((F 2000 "2"), (Score 7 0 3)), ((F 4000 "4"), (Score 8 0 2)), ((F 8000 "8"), (Score 9 0 1)), ((F 16000 "16"), (Score 10 0 0))])
-  --  let currentM = constDyn (Data.Map.fromList [((F 63 "63"), (Score 2 0 8)),((F 125 "125"), (Score 3 0 7)), ((F 250 "250"), (Score 4 0 6)), ((F 500 "500"), (Score 5 0 5)), ((F 1000 "1"), (Score 6 0 4)), ((F 2000 "2"), (Score 7 0 3)), ((F 4000 "4"), (Score 8 0 2)), ((F 8000 "8"), (Score 9 0 1))])
-  --  let currentM = constDyn (Data.Map.fromList [((F 125 "125"), (Score 3 0 7)), ((F 250 "250"), (Score 4 0 6)), ((F 500 "500"), (Score 5 0 5)), ((F 1000 "1"), (Score 6 0 4)), ((F 2000 "2"), (Score 7 0 3)), ((F 4000 "4"), (Score 8 0 2))])
-  --  let currentM = constDyn (Data.Map.fromList [((F 250 "250"), (Score 4 0 6)), ((F 500 "500"), (Score 5 0 5)), ((F 1000 "1"), (Score 6 0 4)), ((F 2000 "2"), (Score 7 0 3))])
-  --  let currentM = constDyn (Data.Map.fromList [((F 500 "500"), (Score 5 0 5)), ((F 1000 "1"), (Score 6 0 4))])
-  --  let currentM = constDyn (Data.Map.fromList [((F 500 "500"), (Score 5 0 5))])
-  --  displayCurrentSpectrumEvaluation (constDyn "Current Performance") currentM
--}
+
+{- x graphLabel qLabel possibilities scoreMap = do
+ listOfScores <- mapDyn ...
+ let qLabel' = Line 400 300 200 200
+ let possibilities = Rect 400 300 200 100
+ scores <- mapDyn (\x -> Line x 300 150 100) listOfScores
+ allDrawingInstructions <- mapDyn (\x -> qLabel':possibilities:x) scores
+ dynSvg allDrawingInstructions -}
