11ba68f5084ad5b3f7159d3c427f2a438ea6edca
Jamie Beverley <jamie_beverley@hotmail.com>
2018-02-21 11:48:55 -0500

    improvements to source widget

--- src/InnerEar/Widgets/Canvas.hs
+++ src/InnerEar/Widgets/Canvas.hs
@@ -14,7 +14,12 @@ import GHCJS.DOM.EventM
 
 import InnerEar.Widgets.Utility
 
-data CanvasEvent = ClickEvent Double | DragEvent Double | ReleaseEvent Double
+data CanvasEvent = ClickEvent {value::Double} | DragEvent {value::Double} | ReleaseEvent {value::Double}
+
+toPercent:: CanvasEvent -> Double -> CanvasEvent
+toPercent (ClickEvent a) b = ClickEvent (a/b)
+toPercent (DragEvent a) b = DragEvent (a/b)
+toPercent (ReleaseEvent a) b = ReleaseEvent (a/b)
 
 data RangeState = RangeState {
     leftRange :: Double,
@@ -31,7 +36,7 @@ updateRangeState (ClickEvent x) (RangeState l r (Just _)) = RangeState l r Nothi
 updateRangeState (DragEvent x) (RangeState l r Nothing) = RangeState l r Nothing  -- If get a drag event when a line isn't selected (hasn't been clicked before) don't drag
 updateRangeState (DragEvent x) (RangeState l r (Just (Left _))) = RangeState l r (Just $ Left x)  -- update drag position if is dragging
 updateRangeState (DragEvent x) (RangeState l r (Just (Right _))) = RangeState l r (Just $ Left x)
-updateRangeState (ReleaseEvent x) (RangeState l r Nothing) = RangeState l r Nothing
+updateRangeState (ReleaseEvent x) (RangeState l r Nothing) = RangeState l r Nothing -- If hasn't been dragging...
 updateRangeState (ReleaseEvent x) (RangeState l r (Just (Left _)))
   | x>=r = RangeState (r-0.02) r Nothing
   | otherwise = RangeState x r Nothing
@@ -41,31 +46,22 @@ updateRangeState (ReleaseEvent x) (RangeState l r (Just (Right _)))
 
 
 --
-rangePicker :: MonadWidget t m => (Double,Double) ->  m (Dynamic t (Double,Double))
-rangePicker (l,r) = do
-  let initialRangeState = RangeState l r Nothing
-  (resize, canvasElement) <- resizeDetector $ liftM (_el_element . fst) $ elClass' "canvas" "startEndCanvas" $ return ()
+rangeSelect :: MonadWidget t m => (Double,Double) ->  m (Dynamic t (Double,Double))
+rangeSelect (l,r) = do
+  let initialRangeState = RangeState l (r) Nothing
+  canvasElement <- liftM (_el_element . fst) $ elClass' "canvas" "startEndCanvas" $ return ()
+  let resize = never
   let htmlCanvasEl = DT.castToHTMLCanvasElement canvasElement -- HTMLCanvasElement
-
-  --      iWidth <- liftIO (getWidth htmlCanvasEl)::MonadWidget t m  => m(Double)
   postBuild <- getPostBuild
-  iWidthEv <- performEvent $ fmap (\_->liftIO (getWidth htmlCanvasEl)) postBuild -- (::MonadWidget t m => m (Event t Double))
-  resizeEv <- performEvent $ fmap (\_->liftIO (getWidth htmlCanvasEl)) resize -- (::MonadWidget t m => m (Event t Double))
+  iWidthEv <- performEvent $ fmap (\_->liftIO (getClientWidth htmlCanvasEl)) postBuild -- (::MonadWidget t m => m (Event t Double))
+  resizeEv <- performEvent $ fmap (\_->liftIO (getClientWidth htmlCanvasEl)) resize -- (::MonadWidget t m => m (Event t Double))
   canvasWidth <- holdDyn 0.5 $ leftmost [iWidthEv, resizeEv]
-
-  clickEv <- liftM ((attachDynWith (\x y-> ClickEvent $ (fromIntegral y)/x) canvasWidth) . fmap fst) $ wrapDomEvent canvasElement (onEventName D.Mousedown) mouseOffsetXY
-  dragEv <- liftM ((attachDynWith (\x y-> DragEvent $ (fromIntegral y)/x) canvasWidth) . fmap fst) $ wrapDomEvent canvasElement (onEventName D.Drag) mouseOffsetXY
-  releaseEv <- liftM ((attachDynWith (\x y-> ReleaseEvent $ (fromIntegral y)/x) canvasWidth) . fmap fst) $ wrapDomEvent canvasElement (onEventName D.Mouseup) mouseOffsetXY
-
-  -- clickEv <- liftM ( (<$>) (ClickEvent . fst)) $ wrapDomEvent canvasElement (onEventName D.Click) mouseOffsetXY
-  -- dragEv <- liftM ( (<$>) (DragEvent . fst)) $ wrapDomEvent canvasElement (onEventName D.Drag) mouseOffsetXY
-  -- releaseEv <- liftM ( (<$>) (ReleaseEvent . fst)) $ wrapDomEvent canvasElement (onEventName D.Drag) mouseOffsetXY
-  -- let dragEv = never
-  -- let clickEv = never
-  -- let releaseEv = never
-  rangeState <- foldDyn updateRangeState  initialRangeState (leftmost [clickEv, dragEv, releaseEv])
-  mapDyn show rangeState >>= dynText
-  -- performEvent_ $ fmap (\x -> liftIO $ drawRange x htmlCanvasEl) (updated rangeState)
+  clickEv <- liftM ( fmap (ClickEvent . fromIntegral . fst )) $ wrapDomEvent canvasElement (onEventName D.Mousedown) mouseOffsetXY
+  dragEv <- liftM (fmap (DragEvent . fromIntegral . fst)) $ wrapDomEvent canvasElement (onEventName D.Drag) mouseOffsetXY
+  releaseEv <- liftM (fmap (ReleaseEvent . fromIntegral . fst)) $ wrapDomEvent canvasElement (onEventName D.Mouseup) mouseOffsetXY
+  getCanvasWidth <- performEvent $ fmap (\_ -> liftIO (getClientWidth htmlCanvasEl)) $ leftmost [clickEv, dragEv, releaseEv]
+  canvasWidth <- holdDyn 0 $ leftmost [getCanvasWidth, iWidthEv]
+  rangeState <- foldDyn updateRangeState  initialRangeState (attachDynWith (flip toPercent) canvasWidth $ leftmost [clickEv, dragEv, releaseEv])
   postBuild <- getPostBuild
   performEvent_ $ fmap (liftIO . drawRange htmlCanvasEl) (leftmost [updated rangeState, initialRangeState <$ postBuild])
   forDyn rangeState $ \x -> (leftRange x, rightRange x)
@@ -75,14 +71,16 @@ drawRange canvas (RangeState l r drag)  = do
   clearCanvas canvas
   w <- getWidth canvas
   h <- getHeight canvas
-  strokeStyle canvas "#ffff00"
+  fillStyle canvas "#ffff00"
   fillRect canvas (l*w) 0 4 h
-  fillRect canvas (r*w) 0 4 h
-
+  fillRect canvas (r*w-4) 0 4 h -- Minus 4 (width of rectangle) so that can still see it when r = 1
+  fillStyle canvas "rgba(255,255,0,0.4)"
+  fillRect canvas 0 0 (l*w) h
+  fillRect canvas (r*w) 0 (w-r*w) h
 
 
 foreign import javascript unsafe
-  "$1.getContext('2d').strokeStyle = $2" strokeStyle :: DT.HTMLCanvasElement -> JSString -> IO ()
+  "$1.getContext('2d').fillStyle = $2" fillStyle :: DT.HTMLCanvasElement -> JSString -> IO ()
 
 foreign import javascript unsafe
   "$1.getContext('2d').clearRect(0,0,$1.width,$1.height)" clearCanvas :: DT.HTMLCanvasElement  -> IO ()
@@ -91,29 +89,10 @@ foreign import javascript unsafe
   "$r = $1.width" getWidth :: DT.HTMLCanvasElement -> IO Double
 
 foreign import javascript unsafe
+  "$r = $1.clientWidth" getClientWidth :: DT.HTMLCanvasElement -> IO Double
+
+foreign import javascript unsafe
   "$r = $1.height" getHeight :: DT.HTMLCanvasElement -> IO Double
 
 foreign import javascript unsafe
  "$1.getContext('2d').fillRect($2,$3,$4,$5)" fillRect :: DT.HTMLCanvasElement -> Double -> Double -> Double -> Double -> IO ()
-
---
--- foreign import javascript unsafe
---   "$1.getContext('2d').strokeStyle = $2" strokeStyle :: DT.HTMLCanvasElement -> T.JSString -> IO ()
---
--- foreign import javascript unsafe
---   "$1.getContext('2d').clearRect(0,0,$1.width,$1.height)" clearCanvas :: DT.HTMLCanvasElement  -> IO ()
---
--- foreign import javascript unsafe
---   "$r = $1.width" getWidth :: DT.HTMLCanvasElement -> IO Float
---
--- foreign import javascript unsafe
---   "$r = $1.height" getHeight :: DT.HTMLCanvasElement -> IO Float
---
--- foreign import javascript unsafe
---  "$1.getContext('2d').fillRect($2,$3,$4,$5)" fillRect :: DT.HTMLCanvasElement -> Float -> Float -> Float -> Float -> IO ()
-
---   postBuild <- getPostBuild -- m (Event t ())
---   let initialRangeEvent = fmap (const (0.25,0.75)) postBuild -- (Event t (Int,Int))
---   let newLeftSelection = fmap (\(x,_) -> fromIntegral(x)/600.0 ) clickEv
---   let newRangeEvent = fmap (\x -> (x,0.75)) newLeftSelection
--- let rangeEvent = leftmost [initialRangeEvent,newRangeEvent]
