83acdfcedc08f19d1df10b5b10668cc1b0b55847
d0kt0r0 <ogbornd@mcmaster.ca>
2018-03-08 18:21:50 -0500

    we're almost there

--- reflex-synth/src/Reflex/Synth/Graph.hs
+++ reflex-synth/src/Reflex/Synth/Graph.hs
@@ -1,9 +1,6 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
 
 module Reflex.Synth.Graph (
-  SourceProps,
-  SourceSinkProps,
-  SinkProps,
   Graph,
   Change,
   Synth,
@@ -12,16 +9,8 @@ module Reflex.Synth.Graph (
   Env,
   buildSynth,
   synthSource,
-  synthNode,
   synthSink,
   audioParamSink,
-  ref,
-  deref,
---  parallelChannels,
---  diverge,
---  branch,
---  merge,
---  mix,
   setParamValue,
   linearRampToParamValue,
   exponentialRampToParamValue,
@@ -51,33 +40,24 @@ instance (Monad m) => MonadUnique (UniqueT m) where
 evalUniqueT :: Monad m => UniqueT m a -> m a
 evalUniqueT (UniqueT s) = evalStateT s 0
 
-type Reference = Integer
-type Env = Map.Map Reference Graph
+type AudioParam = String
 
-data SourceProps
-  = SourceRef Reference
-  | SourceSpec SourceNodeSpec
+data Reference = RefToNode Integer | RefToParamOfNode Integer AudioParam deriving (Show)
+
+data NodeProps =
+  SourceSpec SourceNodeSpec |
+  SourceSinkSpec NodeSpec |
+  SinkSpec SinkNodeSpec
   deriving (Show)
 
-data SourceSinkProps
-  = SourceSinkRef Reference
-  | Parallel [Graph]
-  | SourceSinkSpec NodeSpec
-  deriving (Show)
-
-data SinkProps
-  = SinkParamRef Graph AudioParam
-  | SinkSpec SinkNodeSpec
-  deriving (Show)
+type Env = Map.Map Integer NodeProps
 
 data Graph
-  = Source SourceProps
-  | Sink SinkProps Graph
-  | SourceSink SourceSinkProps Graph
+  = Source Reference
+  | Sink Reference Graph
+  | SourceSink Reference Graph
   | EmptyGraph
   deriving (Show)
-
-type AudioParam = String
 
 data Change
   = SetValue { node :: Graph, param :: AudioParam, value :: Double, endTime :: Time }
@@ -90,6 +70,7 @@ data Synth a = Synth {
     graphs :: ([Graph], [Graph]),
     env :: Env,
     changes :: [Change],
+    deletionTime :: Maybe Time,
     supplement :: a
   } deriving (Show)
 
@@ -103,8 +84,8 @@ connectGraphs EmptyGraph y = y
 connectGraphs x EmptyGraph = x
 connectGraphs x@(Sink _ _) y = error $ "Sink can't be first: " ++ show x ++ " : " ++ show y
 connectGraphs x y@(Source _) = error $ "Source can't be second: " ++ show x ++ " : " ++ show y
-connectGraphs x (SourceSink spec y) = SourceSink spec (connectGraphs x y)
-connectGraphs x (Sink spec y) = Sink spec (connectGraphs x y)
+connectGraphs x (SourceSink r y) = SourceSink r (connectGraphs x y)
+connectGraphs x (Sink r y) = Sink r (connectGraphs x y)
 
 connectGraphs' :: ([Graph], [Graph]) -> Graph -> ([Graph], [Graph])
 connectGraphs' ([], completed) y
@@ -145,146 +126,113 @@ instance Functor Synth where
   fmap f x = x { supplement = f (supplement x) }
 
 instance Applicative Synth where
-  pure x = Synth { graphs = ([], []), env = Map.empty, changes = [], supplement = x }
-  (Synth g1 e1 cs1 f) <*> (Synth g2 e2 cs2 x) = Synth {
+  pure x = Synth { graphs = ([], []), env = Map.empty, changes = [], deletionTime = Nothing, supplement = x }
+  (Synth g1 e1 cs1 d1 f) <*> (Synth g2 e2 cs2 d2 x) = Synth {
     graphs = connectGraphStacks g1 g2,
     env = Map.union e1 e2,
     changes = cs1 ++ cs2,
+    deletionTime = combineDeletionTimes d1 d2,
     supplement = f x
   }
 
 instance Monad Synth where
-  (Synth g1 e1 cs1 a) >>= f = Synth {
+  (Synth g1 e1 cs1 d1 a) >>= f = Synth {
     graphs = connectGraphStacks g1 g2,
     env = Map.union e1 e2,
     changes = cs1 ++ cs2,
+    deletionTime = combineDeletionTimes d1 d2,
     supplement = b
-  } where (Synth g2 e2 cs2 b) = f a
+  } where (Synth g2 e2 cs2 d2 b) = f a
 
-synthOfGraph :: Graph -> a -> SynthBuilder a
-synthOfGraph g s = lift $ Synth {
-  graphs = if isComplete g then ([], [g]) else ([g], []),
-  env = Map.empty,
+combineDeletionTimes :: Maybe Time -> Maybe Time -> Maybe Time
+combineDeletionTimes Nothing Nothing = Nothing
+combineDeletionTimes (Just t) Nothing = Just t
+combineDeletionTimes Nothing (Just t) = Just t
+combineDeletionTimes (Just t1) (Just t2) = Just (max t1 t2)
+
+synthSource :: SourceNodeSpec -> SynthBuilder Graph
+synthSource spec = do
+  r <- fresh
+  lift $ makeSynth r (Source (RefToNode r)) (SourceSpec spec)
+
+synthSourceSink :: NodeSpec -> SynthBuilder Graph
+synthSourceSink spec = do
+  r <- fresh
+  lift $ makeSynth r (SourceSink (RefToNode r) EmptyGraph) (SourceSinkSpec spec)
+
+synthSink :: SinkNodeSpec -> SynthBuilder Graph
+synthSink spec = do
+  r <- fresh
+  lift $ makeSynth r (Sink (RefToNode r) EmptyGraph) (SinkSpec spec)
+
+makeSynth :: Integer -> Graph -> NodeProps -> Synth Graph
+makeSynth r g np = Synth {
+  graphs = ([g],[]),
+  env = Map.singleton r np,
   changes = [],
-  supplement = s
+  deletionTime = Nothing,
+  supplement = g
 }
 
-synthSource :: SourceNodeSpec -> SynthBuilder ()
-synthSource spec = synthOfGraph (Source $ SourceSpec spec) ()
+audioParamSink :: Graph -> AudioParam -> SynthBuilder Graph
+audioParamSink g p = lift $ Synth {
+  graphs = ([g'],[]),
+  env = Map.empty,
+  changes = [],
+  deletionTime = Nothing,
+  supplement = g
+  }
+  where g' = Sink (RefToParamOfNode (getNodeId g) p) EmptyGraph
 
-synthNode :: NodeSpec -> SynthBuilder ()
-synthNode spec = synthOfGraph (SourceSink (SourceSinkSpec spec) EmptyGraph) ()
+getNodeId :: Graph -> Integer
+getNodeId (Source (RefToNode r)) = r
+getNodeId (Sink (RefToNode r) _) = r
+getNodeId (SourceSink (RefToNode r) _) = r
+getNodeId _ = error "you used the f function incorrectly"
 
-synthSink :: SinkNodeSpec -> SynthBuilder ()
-synthSink spec = synthOfGraph (Sink (SinkSpec spec) EmptyGraph) ()
-
-audioParamSink :: Graph -> AudioParam -> SynthBuilder ()
--- TODO only match references here? It doesn't make sense to modulate an isolated node
-audioParamSink g p = synthOfGraph (Sink (SinkParamRef g p) EmptyGraph) ()
-
-splitChannels :: Int -> [SynthBuilder Int]
-splitChannels i = fmap createChannelSource [0..i-1]
-  where
-    createChannelSource x = synthOfGraph EmptyGraph x
-
--- |parallel is a closed fork-join that works on the channels of the input.
---parallelChannels :: Int ->  (Int -> SynthBuilder ()) -> SynthBuilder ()
---parallelChannels i f = lift $ Synth { -- TODO pass supplement through
---    graph = SourceSink (Parallel (fmap (\x -> graph (buildSynth (x >>= f))) (splitChannels i))) EmptyGraph,
---    env = foldl (\x y -> Map.union x $ env (buildSynth (y >>= f))) Map.empty (splitChannels i),
---    changes = foldl (\x y -> x ++ changes (buildSynth (y >>= f))) [] (splitChannels i),
---    supplement = ()
---}
-
-ref :: SynthBuilder a -> SynthBuilder Graph
-ref r = do
-  let (Synth (g:_, []) e cs _) = buildSynth r in do
-    uid <- fresh
-    let reference = case g of
-          (Source _) -> Source (SourceRef uid)
-          (SourceSink _ g') -> SourceSink (SourceSinkRef uid) g'
-          _ -> error $ "Cannot reference " ++ show g
-    synthOfGraph reference ()
-    lift $ Synth {
-      graphs = ([], []),
-      env = Map.insert uid g e,
-      changes = cs,
-      supplement = reference
-    }
-
-deref :: Graph -> SynthBuilder ()
-deref g = synthOfGraph g ()
-
--- |diverge branches away from the main synth but retains references from the
--- original. Divergent forks must eventually connect to a destination which may
--- be the same destination as the top level synth (AudioContext).
---diverge :: SynthBuilder a -> SynthBuilder a
---diverge forkBuilder =
---  let (Synth g e cs s) = buildSynth forkBuilder in do
---    lift $ Synth {
---      graph = Unconnected g EmptyGraph,
---      env = e,
---      changes = cs,
---      supplement = s
---    }
-
---branch :: SynthBuilder a -> SynthBuilder (SynthBuilder a)
---branch branchBuilder =
---  let (Synth g e cs s) = buildSynth branchBuilder in do
---    return $ lift $ Synth {
---      graph = g,
---      env = e,
---      changes = cs,
---      supplement = s
---    }
-
--- |merge is the opposite of `diverge` which merges the nodes.
---merge :: [SynthBuilder a] -> SynthBuilder [a]
---merge branchBuilders =
---  let branches = fmap buildSynth branchBuilders in do
---    lift $ Synth {
---      graph = Join (fmap graph branches) EmptyGraph,
---      env = foldl (\x y -> Map.union x $ env y) Map.empty branches,
---      changes = foldl (\x y -> x ++ (changes y)) [] branches,
---      supplement = fmap supplement branches
---    }
-
---mix :: [SynthBuilder a] -> SynthBuilder [a]
---mix synths =
---  merge $ fmap (\x -> branch x >>= id) synths
-
-change :: Change -> SynthBuilder ()
-change c = lift $ Synth {
+change :: a -> Change -> SynthBuilder a
+change a c = lift $ Synth {
   graphs = ([], []),
   env = Map.empty,
   changes = [c],
-  supplement = ()
+  deletionTime = Nothing,
+  supplement = a
 }
 
-setParamValue :: Graph -> AudioParam -> Double -> Time -> SynthBuilder ()
-setParamValue n p v e = change $ SetValue n p v e
+setParamValue :: Graph -> AudioParam -> Double -> Time -> SynthBuilder Graph
+setParamValue g p v e = change g $ SetValue g p v e
 
-linearRampToParamValue :: Graph -> AudioParam -> Double -> Time -> SynthBuilder ()
-linearRampToParamValue n p v e = change $ LinearRampToValue n p v e
+linearRampToParamValue :: Graph -> AudioParam -> Double -> Time -> SynthBuilder Graph
+linearRampToParamValue g p v e = change g $ LinearRampToValue g p v e
 
-exponentialRampToParamValue :: Graph -> AudioParam -> Double -> Time -> SynthBuilder ()
-exponentialRampToParamValue n p v e = change $ ExponentialRampToValue n p v e
+exponentialRampToParamValue :: Graph -> AudioParam -> Double -> Time -> SynthBuilder Graph
+exponentialRampToParamValue g p v e = change g $ ExponentialRampToValue g p v e
 
-curveToParamValue :: Graph -> AudioParam -> [Double] -> Time -> Time -> SynthBuilder ()
-curveToParamValue n p vs s d = change $ CurveToValue n p vs s d
+curveToParamValue :: Graph -> AudioParam -> [Double] -> Time -> Time -> SynthBuilder Graph
+curveToParamValue g p vs s d = change g $ CurveToValue g p vs s d
 
--- test1 :: ([Graph], [Graph])
+setDeletionTime :: Time -> SynthBuilder ()
+setDeletionTime t = lift $ Synth {
+  graphs = ([], []),
+  env = Map.empty,
+  changes = [],
+  deletionTime = Just t,
+  supplement = ()
+  }
+
 test1 :: Synth ()
 test1 = buildSynth $ do
-  r <- ref $ synthSource $ Oscillator Sine (Hz 440)
-  linearRampToParamValue r "freq" 880 (Sec 4.0) 
+  r <- synthSource $ Oscillator Sine (Hz 440)
+  linearRampToParamValue r "freq" 880 (Sec 4.0)
   synthSource $ Oscillator Sine (Hz 220)
   synthSink Destination
   synthSink Destination
   synthSource $ Oscillator Sawtooth (Hz 110)
   synthSink Destination
+  setDeletionTime (Sec 2.0)
+  return ()
 
 test2 :: Synth ()
 test2 = buildSynth $ do
-  g <- ref $ synthSource $ Oscillator Sine (Hz 440)
+  g <- synthSource $ Oscillator Sine (Hz 440)
   return ()
